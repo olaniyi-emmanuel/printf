c4ace695744f509c5d65e4f61f2c94bf2321b496
olaniyi-emmanuel <emmanuel4sucess@outlook.com>
2023-09-18 12:23:49 -0700

    task

--- _printf.c
+++ _printf.c
@@ -1,7 +1,12 @@
-#include "main.h"
+=#include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
 
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 
 int _printf(const char *format, ...)
 {
@@ -30,8 +35,8 @@ int _printf(const char *format, ...)
 
 				print_buffer(buffer, &buff_ind);
 
-			
-  
+			/* write(1, &format[i], 1);*/
+
 			printed_chars++;
 		}
 
@@ -69,8 +74,11 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
-
-
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 
 void print_buffer(char buffer[], int *buff_ind)
 
